d7faf1f8b5b206100ad076261a63f8f6e0f127c4
Bindu <bindu.koorma@gmail.com>
2023-04-25 15:15:07 +0100

    testcommitapp

---
+++ app/src/test/java/com/vtech/services/AdminServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vtech.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class AdminServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ app/target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into student values(10001,'Alex', 'E1234567');
+insert into student values(10002,'Natasha', 'A1234568');
